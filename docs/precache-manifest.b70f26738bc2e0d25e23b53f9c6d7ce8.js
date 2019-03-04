918fa1358cd5b931cd96919394bb81ed90693844
ashanti <as609@mtec.cf>
2019-03-04 17:09:43 +0900

    1st commit

---
+++ docs/precache-manifest.b70f26738bc2e0d25e23b53f9c6d7ce8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "66928b341f46be88c5f3",
+    "url": "js/chunk-vendors.ad7ab701.js"
+  },
+  {
+    "revision": "6b68980b1e9e86a566de",
+    "url": "js/app.333da9e7.js"
+  },
+  {
+    "revision": "48af24cd7dae7280ac56",
+    "url": "js/about.41d636ee.js"
+  },
+  {
+    "revision": "694e4beb34d2854e940a46f16700212f",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "66928b341f46be88c5f3",
+    "url": "css/chunk-vendors.1b8a695f.css"
+  },
+  {
+    "revision": "6b68980b1e9e86a566de",
+    "url": "css/app.2a57277c.css"
+  }
+];
